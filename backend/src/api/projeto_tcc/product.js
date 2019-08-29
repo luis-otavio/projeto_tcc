3e5bacf47188ad4767a171ebd51f5ad62322378b
luis-otavio <luisotavio05@gmail.com>
2019-08-29 17:45:48 -0300

    Adicionando o cors

--- backend/src/api/projeto_tcc/product.js
+++ backend/src/api/projeto_tcc/product.js
@@ -5,16 +5,16 @@ const productSchema = new mongoose.Schema({
     description: { type: String, require: true },
     modelo: { type: String, require: true },
     couro: { type: String, require: true },
-    qt_couro: { type: String, require: true },
+    qt_couro: { type: Number, require: true },
     metal: { type: String, require: true },
-    qt_metal: { type: String, require: true },
+    qt_metal: { type: Number, require: true },
     forro: { type: String, require: true },
-    qt_forro: { type: String, require: true },
+    qt_forro: { type: Number, require: true },
     ziper: { type: String, require: true },
-    qt_ziper: { type: String, require: true },
-    vl_montagem: { type: String, require: true },
-    vl_tear:  { type: String, require: true },
-    total_pro: { type: String, require: true }
+    qt_ziper: { type: Number, require: true },
+    vl_montagem: { type: Number, require: true },
+    vl_tear:  { type: Number, require: true },
+    total_pro: { type: Number, require: true }
 })
 
 module.exports = restful.model('Produto', productSchema)

61afda90c1953bb6bdaa10fd386a691c43074fc0
luis-otavio <luisotavio05@gmail.com>
2019-08-30 12:31:47 -0300

    Adicionando componente Menu

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -4,9 +4,11 @@ import 'modules/font-awesome/css/font-awesome.min.css'
 import React from 'react'
 import Product from '../product/product'
 import About from '../about/about'
+import Menu from '../template/menu'
 
 export default props => (
     <div className='container'>
+        <Menu />
         <Product />
         <About />
     </div>
---
+++ frontend/src/template/menu.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default props => (
+    <nav className='navbar navbar-inverse bg-inverse'>
+        <div className='container'>
+            <div className='navbar-header'>
+                <a className='navbar-brand' href='#'>
+                    <i className='fa fa-edit'></i> CadSystem
+                </a>
+            </div>
+
+            <div id='navbar' className='navbar-collapse collapse'>
+                <ul className='nav navbar-nav'>
+                    <li><a href='#/products'>Produtos</a></li>
+                    <li><a href='#/about'>Sobre</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+)

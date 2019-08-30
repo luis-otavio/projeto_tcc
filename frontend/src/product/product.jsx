763a6aba92f1a0ae16c0ec9f7176a91286574929
luis-otavio <luisotavio05@gmail.com>
2019-08-30 16:23:59 -0300

    Adicionando componentes Form e List

--- frontend/src/product/product.jsx
+++ frontend/src/product/product.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import PageHeader from '../template/pageHeader'
+import ProductForm from './productForm'
+import ProductList from './productList'
 
 export default class Product extends Component {
     render() {
         return (
             <div>
                 <PageHeader name='Produtos' small='Cadastro'></PageHeader>
+                <ProductForm />
+                <ProductList />
             </div>
         )
     }

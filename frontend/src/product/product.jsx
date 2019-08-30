171cc00e97301fbdb37868d30ff477e844a8c1a8
luis-otavio <luisotavio05@gmail.com>
2019-08-30 12:43:27 -0300

    Adicionando componente PageHeader

--- frontend/src/product/product.jsx
+++ frontend/src/product/product.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default class Product extends Component {
     render() {
         return (
             <div>
-                <h1>Produto</h1>
+                <PageHeader name='Produtos' small='Cadastro'></PageHeader>
             </div>
         )
     }

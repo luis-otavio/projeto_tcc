f0552675c4898d9047fc38f7f71a2999e4f5aafd
luis-otavio <luisotavio05@gmail.com>
2019-09-15 16:10:54 -0300

    Update material

--- backend/src/api/projeto_tcc/material.js
+++ backend/src/api/projeto_tcc/material.js
@@ -2,6 +2,11 @@ const restful = require('node-restful')
 const mongoose = restful.mongoose
 
 const materialSchema = new mongoose.Schema({
+    product: { 
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Produto',
+        require: true,
+    },
     mat_tipo: { type: String, require: true },
     mat_nome: { type: String, require: true },
     mat_custo: { type: Number, require: true },

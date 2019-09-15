3a2d558ab655435d4cabcf14a932058751225c74
luis-otavio <luisotavio05@gmail.com>
2019-09-15 16:10:48 -0300

    Update product

--- backend/src/api/projeto_tcc/product.js
+++ backend/src/api/projeto_tcc/product.js
@@ -4,6 +4,15 @@ const mongoose = restful.mongoose
 const productSchema = new mongoose.Schema({
     description: { type: String, require: true },
     modelo: { type: String, require: true },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Material',
+        require: true,
+    },
+    valores: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Material',
+    }],
     vl_montagem: { type: Number, require: false },
     vl_tear:  { type: Number, require: false },
     createdAd: { type: Date, default: Date.now }
